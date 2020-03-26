12a3e36cbd6f078d47e65a3b7880ca49da75f20e
bnibler <blake.nibler@sdvi.com>
2020-03-26 09:13:31 -0700

    Updated ids

--- selenium_sample.py
+++ selenium_sample.py
@@ -2,11 +2,11 @@ def test_step(self):
     driver = self.driver
     driver.get("https://dev.sdvi.com")
 
-    driver.find_element_by_id('tab1Id').click()
-    descriptionElement = driver.find_element_by_id('resource1TabId')
+    driver.find_element_by_id('resource1TabId').click()
+    descriptionElement = driver.find_element_by_id('resourceDescriptionId')
     descriptionText = descriptionElement.text
     self.assertTrue("Description of resource 1" in descriptionText)
 
-    driver.find_element_by_id('resource1TabId').click()
+    driver.find_element_by_id('resource2TabId').click()
     descriptionText = descriptionElement.text
     self.assertTrue("Description of resource 2" in descriptionText)
